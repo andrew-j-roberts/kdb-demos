c6048036a74c85dcf14e7a6ff8a2e2da20d8070b
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-07-14 17:22:52 -0400

    add optional chaining plugin

--- rollup.config.js
+++ rollup.config.js
@@ -52,6 +52,7 @@ export default {
           ],
           plugins: [
             "@babel/plugin-syntax-dynamic-import",
+            "@babel/plugin-proposal-optional-chaining",
             [
               "@babel/plugin-transform-runtime",
               {
